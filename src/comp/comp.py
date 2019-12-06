4a1c6b81bd062ea7090d36908f86f44d867eaee1
Adetunji <Ashennaike@gmail.com>
2019-12-06 12:15:23 -0500

    comp complete, added the +5 on age in g

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,14 +54,14 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [name for name in humans if 27 <= name.age <= 32]
+f = [(name.name, name.age) for name in humans if 27 <= name.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [name.name.upper() for name in humans]
+g = [Human(name.name.upper(), name.age+5) for name in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
